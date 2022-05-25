1a8c1199f060c6fe281f41fcaef7013cd3f6f2d5
wuchin <wuchin1997@gmail.com>
2022-05-25 16:03:15 +0200

    formate code

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify ,render_template
+from flask import Flask, jsonify, render_template
 from pymongo import MongoClient
 import json
 
 from flask import jsonify
-#https://pymongo.readthedocs.io/en/stable/tutorial.html
-#https://matplotlib.org/
+
+# https://pymongo.readthedocs.io/en/stable/tutorial.html
+# https://matplotlib.org/
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ app = Flask(__name__)
 @app.route('/')
 # this function return data in a plot format
 def main():  # put application's code here
-    #create a list of data to pass to the template
+    # create a list of data to pass to the template
     data = [
         {'name': 'Alice', 'age': '25'},
         {'name': 'Bob', 'age': '27'},
@@ -21,9 +22,10 @@ def main():  # put application's code here
     # render the template with the data
     return render_template('index.html', data=data)
 
+
 @app.route('/test')
 def test():
-    f = open('./owid-covid-data.json','r')
+    f = open('./owid-covid-data.json', 'r')
     datas = json.load(f)
     # for data in datas:
     #     print(data)
@@ -31,20 +33,19 @@ def test():
     return render_template('test.html', data=[datas])
 
 
-@app.route('/api/pays',methods=['GET','POST'])
+@app.route('/api/pays', methods=['GET', 'POST'])
 def api():
-
-
-    f = open('./owid-covid-data.json','r')
+    f = open('./owid-covid-data.json', 'r')
     datas = json.load(f)
 
     return_data = dict()
     i = 0;
     for data in datas:
         return_data[i] = data
-        i = i+1
+        i = i + 1
 
     return jsonify(return_data)
+
 
 # FLASK_ENV="development"
 if __name__ == '__main__':
