b1ec9ad3c4bfa134bd9e0742a5b5d1519990c455
wuchin <wuchin1997@gmail.com>
2022-05-27 12:54:48 +0200

    end projet

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify, render_template, request , make_response
+from flask import Flask, jsonify, render_template, request , make_response , redirect
 from hashlib import blake2b
+from hmac import compare_digest
 import json
+
+from datetime import datetime
 
 from flask import jsonify
 
@@ -10,26 +13,36 @@ from flask import jsonify
 def generateToken(user_login):
     return blake2b(b"{user_login}").hexdigest();
 
-def log_information(user,fun):
-    log_file = open('./logs.log','w+')
-    log_file.write(f'INFO - {user=} - date - function')
+def log_information(user):
+    log_file = open('./logs.log','a+')
+    date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    log_file.write(f'INFO - {user=} - {date} - {request.path} \n')
     log_file.close()
 
 
 
 def checkTokenValidation(user_login,token):
-    return compare_digest(blake2b(b'user_name:qsd,password:123').hexdigest(),res)
+    return compare_digest(blake2b(b'user_name:qsd,password:123').hexdigest(),token)
 
+def token_verifie(f):
+    def decorator(*args, **kwargs):
+        token = None
+        # ensure the jwt-token is passed with the headers
 
-def token_verifie(func):
-    def wrapper(*args,**kwargs):
         cki_user_login =request.cookies.get('user_login')
         cki_token =request.cookies.get('token')
+
+        if not cki_token: # throw error if no token provided
+            return make_response(jsonify({"message": "A valid token is missing!"}), 401)
+
         if checkTokenValidation(cki_user_login ,cki_token) == False:
             redirect('/login')
-        # log_information(user_login,func)
+        log_information(cki_user_login)
 
-    return wrapper
+        return f(*args, **kwargs)
+    return decorator
+
+
 
 app = Flask(__name__)
 
